5c6b77eebfc34d8e0cfe615ecfbeab142a13292e
loloffs <lukeoloffs@gmail.com>
2021-01-27 23:57:45 +0000

    Form submission with jQuery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,9 @@
 $(document).ready(function() {
-  
-  
-  
-  console.log("test");
-
-
-
   $("#tweet-text").on("input", function () {
     let tweetLength = $(this).val().length;
     let counter = $(this).parent().find(".counter");
-
-
     counter.text(140 - tweetLength);
-
     // make counter RED if below 0
-
-
   });
-
-
 });
 
